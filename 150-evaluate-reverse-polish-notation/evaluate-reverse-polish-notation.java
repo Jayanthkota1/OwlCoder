ebea06ec4456e49088bfb4f48326d871ef877392
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2024-01-30 17:16:58 +0530

    Time: 6 ms (78.23%) | Memory: 44.9 MB (20.90%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,28 +2,31 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> s=new Stack<Integer>();
         for(int i=0;i<tokens.length;i++){
-            if(tokens[i].equals("+")||tokens[i].equals("*")||tokens[i].equals("/")||tokens[i].equals("-")){
-                       int a=Integer.valueOf(s.pop());
-                       int b=Integer.valueOf(s.pop());
-                       //System.out.print(a+" "+b+"\n");
                        if(tokens[i].equals("+")) {
+                           int a=Integer.valueOf(s.pop());
+                           int b=Integer.valueOf(s.pop());
                            s.push(a+b);
                            //System.out.print(a+b+"\n");
                            }
                        else if(tokens[i].equals("-")) {
+                           int a=Integer.valueOf(s.pop());
+                           int b=Integer.valueOf(s.pop());
                            s.push(b-a);
                             //System.out.print(a-b+"\n");
                             }
                        else if(tokens[i].equals("/")) {
+                           int a=Integer.valueOf(s.pop());
+                           int b=Integer.valueOf(s.pop());
                            s.push(b/a);
                            // System.out.print(a/b+"\n");
                            }
-                       else {
-                           s.push(a*b);
+                       else if(tokens[i].equals("*")) {
+                           int a=Integer.valueOf(s.pop());
+                           int b=Integer.valueOf(s.pop());
+                           s.push(b*a);
                            // System.out.print(a*b+"\n");
                            }
-            }
-            else s.push(Integer.valueOf(tokens[i]));
+                       else s.push(Integer.valueOf(tokens[i]));
         }
         return s.pop();
     }
