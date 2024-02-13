6ceda5357c19fbcbf4d410893385eaa1ac25bef2
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2024-02-13 18:23:32 +0530

    Time: 2 ms (71.53%) | Memory: 44.9 MB (65.40%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.java
@@ -1,5 +1,8 @@
 class Solution {
     boolean ispal(String s){
+         if(s== null || s.length() == 0){
+            return false;
+         }
         for(int i=0;i<(s.length()/2)+1;i++) if(s.charAt(i)!=s.charAt(s.length()-i-1)) return false;
         return true;
     }
