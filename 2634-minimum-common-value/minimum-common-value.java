112c5cd030ae120f99bcfa1d460056411a77c90e
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2024-03-09 16:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 62.3 MB (88.00%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -3,6 +3,8 @@ class Solution {
         int i=0,j=0;
         //Arrays.sort(nums2);
         //HashMap<Integer,Integer> a = new HashMap<>();
+        if (nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0])
+            return -1;
         while(i<nums1.length && j<nums2.length){
             if(nums1[i]==nums2[j]) return nums1[i];
             else if(nums1[i]<nums2[j]) i++;
