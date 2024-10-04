692ff8919b661b9b9081492fa892c980ab1d838f
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2024-10-04 11:31:18 +0530

    Time: 27 ms (7.56%) | Memory: 61.1 MB (5.29%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,21 +1,39 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        int n=skill.length;
-        Arrays.sort(skill);
-        int chem=skill[0]+skill[n-1],j=n-2;
-        long res=skill[0]*skill[n-1];
-        for(int i=1;i<n/2;i++,j--)
+        int n=skill.length,min=Integer.MAX_VALUE,max=Integer.MIN_VALUE,count=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++)
         {
-            //System.out.print(skill[i]+" "+skill[j]+"\n");
-            if(skill[i]+skill[j]==chem)
+            min=Math.min(min,skill[i]);
+            max=Math.max(max,skill[i]);
+        }
+        int req=min+max;
+        long res=0;
+        for(int i=0;i<n;i++)
+        {
+           // System.out.print(req-skill[i]+"\n");
+            int key=req-skill[i];
+            if(map.containsKey(key) && map.get(key)>0)
             {
-                   res+=(skill[i]*skill[j]);
+                   count+=1;
+                   res+=(skill[i]*key);
+                   map.put(key,map.get(key)-1);
             }
             else
             {
-                return -1;
+                if(map.containsKey(skill[i]))
+                {
+                     map.put(skill[i],map.get(skill[i])+1);
+                }
+                else
+                {
+                map.put(skill[i],1);
+                }
+               // System.out.print(map);
             }
         }
+     //   System.out.print(count);
+        if(count!=n/2) return -1;
         return res;
     }
 }
